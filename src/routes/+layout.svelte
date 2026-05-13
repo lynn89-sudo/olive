58583f5ac7dbf9cda83e976f62b5a4630cd8ff68
Lynn <luke87joseph@gmail.com>
2026-05-13 21:40:38 +0000

    Init SvelteKit + Fonts

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	import favicon from '$lib/assets/favicon.svg';
+
+	import "$lib/styles.css";
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>Olive Garden :P</title>
+	<link rel="icon" href={favicon} />
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&family=Gamja+Flower&family=Indie+Flower&family=Oleo+Script:wght@400;700&family=Playwrite+CA:wght@100..400&display=swap" rel="stylesheet">
+</svelte:head>
+
+{@render children()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    let fontCycle = ["Gamja Flower", "Indie Flower", "Oleo Script", "Playwrite CA"];
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
